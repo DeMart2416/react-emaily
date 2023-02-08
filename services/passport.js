f12cfb578ca995adcd5f121a87838f19b6f5dc62
Sean DesMarteau <sean.desmarteau@gusto.com>
2023-02-07 22:31:03 -0700

    Async Refactor

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,6 @@ const GoogleStategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
@@ -11,9 +10,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
+    User.findById(id).then((user) => {
         done(null, user);
-    })
+    });
 });
 
 passport.use(
@@ -22,20 +21,17 @@ passport.use(
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: "/auth/google/callback",
-            proxy: true
+            proxy: true,
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({
-                        googleId: profile.id,
-                    })
-                        .save()
-                        .then((user) => done(null, user));
-                }
-            });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
         }
     )
 );
